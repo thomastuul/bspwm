c1ce161099eaec5a91c2b21ef3e4dfa6b00a24b7
Thomas Tuul <thomas.tuul@online.de>
2025-08-14 20:22:33 +0200

    Bugfix for background color of trayer which shall be in form of 0xFFFFFF, not #FFFFFF

--- trayer/start_trayer.sh
+++ trayer/start_trayer.sh
@@ -2,9 +2,12 @@
 
 source "$HOME/.config/bspwm/lemonbar/config.sh"
 
+color="$Background"
+color="0x${color#\#}"
+
 trayer --edge top --align right --SetDockType true \
  --SetPartialStrut true --expand true --transparent true \
- --alpha 1 --tint $COLOR_DEFAULT_BG --widthtype request  \
+ --alpha 1.0 --tint $color --widthtype request  \
  --width 3 --height $PANEL_HEIGHT --distancefrom right --distance 35
 
 
